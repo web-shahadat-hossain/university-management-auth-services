fdc8024cd887439227a61b2c9f465082c4f3bd80
web-shahadat-hossain <mdridoysikder862@gmail.com>
2023-05-29 16:20:57 +0600

    setup express js mongose done

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
+
+// cors
+app.use(cors());
+
+//data parse
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// testing api
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World!");
+  next();
+});
+
+export default app;
